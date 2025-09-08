ef02b16cb9dd46eff827941943a5a6b4587e80bb
ChrisChampGit <140940856+ChrisChampGit@users.noreply.github.com>
2025-09-08 21:27:48 +0800

    Update C5Assignment.py

--- C5Assignment.py
+++ C5Assignment.py
@@ -1,1 +1,12 @@
+# QUESTION 1
 
+import yfinance as yf
+
+Tesla = yf.Ticker("TSLA")
+
+tesla_data = Tesla.history(period="max")
+
+tesla_data.head()
+tesla_data.reset_index(inplace=True)
+
+print(tesla_data)
